0548d57ee44d106d9b0bec053e156cd89bdc9e91
Sahil Sharma <sahilaug@gmail.com>
2021-07-15 10:45:40 +0530

    more renaming

--- Cards.ts
+++ Cards.ts
@@ -3,22 +3,18 @@ const suits = ['club', 'heart', 'spade', 'diamond'] as const;
 
 type Rank = typeof ranks[number];
 type Suit = typeof suits[number];
-
 type Card = {
   rank: Rank,
   suit: Suit
 }
 
 const getSuit = (card: string) => suits.find(suit => card.toLowerCase().includes(suit)) as Suit;
-const getRank = (card: string) => ranks.find(cardRank => card.toLowerCase().includes(cardRank)) as Rank;
-
-const processInput = (card: string) => ({ rank: getRank(card), suit: getSuit(card) });
+const getRank = (card: string) => ranks.find(rank => card.toLowerCase().includes(rank)) as Rank;
+const parseInput = (card: string) => ({ rank: getRank(card), suit: getSuit(card) });
 
 const getWeight = <T>(array: readonly T[], rank: T) => array.indexOf(rank);
-
 const isGreaterRank = (card1: Card, card2: Card) => getWeight(ranks, card1.rank) > getWeight(ranks, card2.rank);
 const isGreaterSuit = (card1: Card, card2: Card) => getWeight(suits, card1.suit) > getWeight(suits, card2.suit);
-
 const isRankEqual = (card1: Card, card2: Card) => getWeight(ranks, card1.rank) === getWeight(ranks, card2.rank);
 const isSuitEqual = (card1: Card, card2: Card) => getWeight(suits, card1.suit) === getWeight(suits, card2.suit);
 
@@ -31,8 +27,8 @@ const getComparison = (card1: Card, card2: Card) => {
 };
 
 export const compare = (cardInput1: string, cardInput2: string) => {
-  const card1 = processInput(cardInput1);
-  const card2 = processInput(cardInput2);
+  const card1 = parseInput(cardInput1);
+  const card2 = parseInput(cardInput2);
   const comparison = getComparison(card1, card2);
   return `${cardInput1} ${comparison} ${cardInput2}`;
 };
